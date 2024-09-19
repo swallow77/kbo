e6df152a1b53dc1d850db5871061fbb9b10be47c
swallow77 <57874611+swallow77@users.noreply.github.com>
2024-09-19 20:41:02 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
+"""The KBO Scores integration."""
+from homeassistant import config_entries, core
 
+async def async_setup(hass: core.HomeAssistant, config: dict):
+    """Set up the KBO Scores integration."""
+    return True
